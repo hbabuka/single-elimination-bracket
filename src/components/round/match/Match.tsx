5dd55206f4ae9ffbd653c78560e4d6ba62ba9b06
hbabuka <hristijan.babuka@gmail.com>
2022-09-17 21:01:55 +0200

    Resolve loser or winner for styling

--- src/components/round/match/Match.tsx
+++ src/components/round/match/Match.tsx
@@ -8,11 +8,22 @@ interface Props {
 
 export const Match = ({ matchData }: Props): ReactElement => {
   const teamsData: TeamsModel[] = matchData.teams
+
+  // Function to resolve the winner or the loser of the match
+  // Needed primarily for different styling
+  const resolveType = (score: number) => {
+    const scoresArray = teamsData.map((team) => team.score)
+    const winner = Math.max(...scoresArray)
+
+    if (score === winner) {
+      return 'WINNER'
+    } else return 'LOSER'
+  }
   
   return (
     <div className="match-wrapper">
       {teamsData.map((team, index) =>
-        <Team teamData={team} type="LOSER" key={index} />
+        <Team teamData={team} type={resolveType(team.score)} key={index} />
       )}
     </div>
   )
